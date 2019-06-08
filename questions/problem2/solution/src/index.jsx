fe699ac9f9ca8b958d7a99218c446eb5e12e83fa
Jordan - Instructor <jordan-instructor@jordans-mbp.wireless.concordia.ca>
2019-06-08 10:07:49 -0400

    Add problem 2 solution

---
+++ questions/problem2/solution/src/index.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom';
+import './main.css';
+import React from 'react';
+import DatingProfile from './DatingProfile.jsx';
+import { datingProfiles } from './data';
+
+function App(props) {
+  return (
+    <div>
+      {datingProfiles.map((datingProfile) => (
+        <DatingProfile
+          key={`${datingProfile.name} ${datingProfile.gender}`}
+          datingProfile={datingProfile}
+        />
+      ))}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
